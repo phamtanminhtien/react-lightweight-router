867d088dc82c4ae688d67d7912f35423c32974c5
Pham Tan Minh Tien <phamtanminhtien@gmail.com>
2025-07-28 14:52:34 +0700

    chore: Bump version to 1.1.5, update file paths in package.json, and enhance Vite configuration for better type support and output naming

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,31 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [react(), dts()],
+    plugins: [react(), dts({
+        insertTypesEntry: true,
+        tsconfigPath: './tsconfig.json',
+        rollupTypes: true
+    })],
     build: {
         lib: {
             entry: 'src/index.ts',
             name: 'LightweightRouter',
             formats: ['es', 'umd'],
-            fileName: (format) => `index.${format}.js`,
+            fileName: (format) => `react-lightweight-router.${format}.js`,
         },
         rollupOptions: {
-            external: ['react', 'react-dom'],
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
             output: {
                 globals: {
                     react: 'React',
-                    'react-dom': 'ReactDOM'
-                }
+                    'react-dom': 'ReactDOM',
+                    'react/jsx-runtime': 'React',
+                },
+                extend: true,
+                inlineDynamicImports: true,
             }
         },
-        outDir: 'dist',
+        sourcemap: true,
         minify: true,
     },
 });
